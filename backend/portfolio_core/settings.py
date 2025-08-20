4137823c58fcf26dca4c3014031cad757c41cf5d
Mahesh Das <Mahesh.das2205@gmail.com>
2025-08-20 13:02:36 +0530

    fix: update settings.py to use environment variables for SECRET_KEY and adjust ALLOWED_HOSTS; modify CORS settings

--- backend/portfolio_core/settings.py
+++ backend/portfolio_core/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ALL_ORIGINS = True
 
 
 ROOT_URLCONF = 'portfolio_core.urls'
